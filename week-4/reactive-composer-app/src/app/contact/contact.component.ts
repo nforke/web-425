e3af21b65e71a520bdf0444a4308ca3736cb9431
nforke <n.forke@gmail.com>
2020-07-30 21:09:54 -0500

    Adding week-4

--- week-4/reactive-composer-app/src/app/contact/contact.component.ts
+++ week-4/reactive-composer-app/src/app/contact/contact.component.ts
@@ -1,11 +1,11 @@
 /*
-=======================================================================
-; Title: Assignment 4.2 - Inversion of Control and Dependency Injection
+================================================================
+; Title: Assignment 4.3 - Handling Form Events with Observables
 ; Author: Nicole Forke
-; Date: 28 July 2020
+; Date: 30 July 2020
 ; Modified By: Nicole Forke
 ; Description: Contact Component
-========================================================
+================================================================
 */
 // import component
 import { Component, OnInit } from '@angular/core';
