da9b6365aaa4486fa7a032829390deacd72139b4
Nicole Forke <n.forke@gmail.com>
2020-08-15 20:20:51 -0500

    Initial commit for assignment 6.4

--- week-6/gpa-calculator-app2/src/app/not-found/not-found.component.ts
+++ week-6/gpa-calculator-app2/src/app/not-found/not-found.component.ts
@@ -1,6 +1,6 @@
 /*
 ================================================================
-; Title: Assignment 6.3 - Output Properties
+; Title: Assignment 6.4 - Input Properties
 ; Author: Nicole Forke
 ; Date: 15 August 2020
 ; Modified By: Nicole Forke
