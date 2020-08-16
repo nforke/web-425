359f71357c15f24a9c115077289b5ca5da5f856c
Nicole Forke <n.forke@gmail.com>
2020-08-15 19:52:10 -0500

    Initial commit for assignment 6.4

---
+++ week-6/gpa-calculator-app2/src/app/app.component.ts
@@ -0,0 +1,20 @@
+/*
+================================================================
+; Title: Assignment 6.4 - Input Properties
+; Author: Nicole Forke
+; Date: 15 August 2020
+; Modified By: Nicole Forke
+; Description: App Component
+================================================================
+*/
+// import component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gpa-calculator-app1';
+}
---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,14 @@
+/*
+================================================================
+; Title: Assignment 6.4 - Input Properties
+; Author: Nicole Forke
+; Date: 15 August 2020
+; Modified By: Nicole Forke
+; Description: Transcript Interface
+================================================================
+*/
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
