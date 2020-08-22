f8fb754a15f78344c16c1707aec717edd88e1ba2
Nicole Forke <n.forke@gmail.com>
2020-08-21 21:36:50 -0500

    Initial Commit for Week-7

--- week-7/gpa-calculator-app3/src/app/app.component.ts
+++ week-7/gpa-calculator-app3/src/app/app.component.ts
@@ -1,8 +1,8 @@
 /*
 ================================================================
-; Title: Assignment 6.4 - Input Properties
+; Title: Assignment 7.2 - Reactive Forms
 ; Author: Nicole Forke
-; Date: 15 August 2020
+; Date: 20 August 2020
 ; Modified By: Nicole Forke
 ; Description: App Component
 ================================================================
--- week-7/gpa-calculator-app3/src/main.ts
+++ week-7/gpa-calculator-app3/src/main.ts
@@ -1,8 +1,8 @@
 /*
 ================================================================
-; Title: Assignment 6.3 - Output Properties
+; Title: Assignment 7.2 - Reactive Forms
 ; Author: Nicole Forke
-; Date: 15 August 2020
+; Date: 20 August 2020
 ; Modified By: Nicole Forke
 ; Description: Main
 ================================================================
